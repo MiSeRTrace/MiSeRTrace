92ab4f8a1bbef56ee97aecf3acccaac326e35349
ThrivikramanV <thrivikraman.vs@gmail.com>
2022-01-03 21:57:06 +0530

    Changed record parsing logic

--- src/core/tracerecord.py
+++ src/core/tracerecord.py
@@ -1,37 +1,18 @@
 class TraceRecord:
     def __init__(self, line: str):
-        # <idle>-0     [016] 56701.429653: sched_switch:          prev_comm=swapper/16 prev_pid=0 prev_prio=120 prev_state=0 next_comm=mongod next_pid=23939 next_prio=120
+        # comm 100 9999999999999 tracepoint:sched:sched_process_fork parent_pid=100 child_pid=101
         lineContents = line.strip().split()
-        self.command = "-".join(lineContents[0].split("-")[:-1])
-        self.pid = int(lineContents[0].split("-")[-1])  # 0 is command-pid
-        self.cpu = int(lineContents[1][1:-1])  # 1 is cpu in the format [cpuNo]
-        self.timeStamp = float(lineContents[2][:-1])  # 2 is timestamp with ':'
-        self.event = lineContents[3][:-1]  # 3 is the event with ':'
-        self.details = (
-            dict()
-        )  # 4 is the dictionary to store all attributes of the event
-        for content in lineContents[4:]:
-            # In case there isn't any key value pair
-            prev = None
-            try:
-                if "=" in content:
+        self.command = lineContents[0]
+        self.pid = int(lineContents[1])
+        self.timeStamp = int(lineContents[2])
+        self.event = lineContents[3].split(":")[-1]
+        self.details = dict()
+        if len(lineContents) > 4:
+            for content in lineContents[4:]:
+                try:
                     attribute, value = content.split("=")
                     self.details[attribute] = value
-                    prev = self.details[attribute]
-                elif prev:
-                    prev += f" {content}"
-            except:
-                print(line)
-                print("Potential error in trace record parsing")
-                exit()
-
-        if self.event in ("tcp_rcv_space_adjust", "tcp_probe", "inet_sock_set_state"):
-            self.details["saddr"] = self.extractAddr("saddr")
-            self.details["daddr"] = self.extractAddr("daddr")
-
-    def extractAddr(self, addrType):
-        addr = self.details[addrType][6:-1].split(",")
-        if len(addr) == 4:
-            return ",".join(addr)
-        else:
-            return ",".join(addr[4:8])
+                except:
+                    print(line)
+                    print("Potential error in trace record parsing")
+                    exit()
